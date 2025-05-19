d134e55bf388547222188b635822acb9067fb28a
moriyan8 <skills.done_0n@icloud.com>
2025-05-20 08:28:07 +0900

    add:投稿詳細ページ作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  validates :image, presence: true
+  validates :fish_name, presence: true
+  validates :fishing_date, presence: true
+
   enum weather: { sunny: 0, cloudy: 1, rainy: 2, snowy: 3 }
   enum status: { post: 0, record: 1 }
 end
