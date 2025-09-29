f524691418863107b11634d6306f7eb74cab51d9
moriyan8 <skills.done_0n@icloud.com>
2025-09-29 14:15:22 +0900

    add:サービス説明に画像を追加

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,3 +5,11 @@
 //= link pin_post.png
 //= link pin_record.png
 //= link fishdrip_ogp.png
+//= link home.jpg
+//= link posts:show.jpg
+//= link posts:new1.png
+//= link posts:new2.png
+//= link posts:map.png
+//= link users:show.jpg
+//= link nft.png
+
